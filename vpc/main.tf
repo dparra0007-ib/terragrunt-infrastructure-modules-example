544e0ca967f802467fcddba56390ff0c270f64eb
david-iaggbs <david.parra@iaggbs.com>
2020-04-20 13:11:04 +0200

    remove Interpolation-only expressions

--- vpc/main.tf
+++ vpc/main.tf
@@ -39,57 +39,57 @@ data "aws_availability_zones" "available" {}
 
 # NETWORKING #
 resource "aws_vpc" "vpc" {
-  cidr_block = "${var.network_address_space}"
+  cidr_block = var.network_address_space
   enable_dns_hostnames = "true"
 
 }
 
 resource "aws_internet_gateway" "igw" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
 }
 
 resource "aws_subnet" "subnet1" {
-  cidr_block        = "${var.subnet1_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.subnet1_address_space
+  vpc_id            = aws_vpc.vpc.id
   map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[0]}"
+  availability_zone = data.aws_availability_zones.available.names[0]
 
 }
 
 resource "aws_subnet" "subnet2" {
-  cidr_block        = "${var.subnet2_address_space}"
-  vpc_id            = "${aws_vpc.vpc.id}"
+  cidr_block        = var.subnet2_address_space
+  vpc_id            = aws_vpc.vpc.id
   map_public_ip_on_launch = "true"
-  availability_zone = "${data.aws_availability_zones.available.names[1]}"
+  availability_zone = data.aws_availability_zones.available.names[1]
 
 }
 
 # ROUTING #
 resource "aws_route_table" "rtb" {
-  vpc_id = "${aws_vpc.vpc.id}"
+  vpc_id = aws_vpc.vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
+    gateway_id = aws_internet_gateway.igw.id
   }
 }
 
 resource "aws_route_table_association" "rta-subnet1" {
-  subnet_id      = "${aws_subnet.subnet1.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rtb.id
 }
 
 resource "aws_route_table_association" "rta-subnet2" {
-  subnet_id      = "${aws_subnet.subnet2.id}"
-  route_table_id = "${aws_route_table.rtb.id}"
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.rtb.id
 }
 
 # SECURITY GROUPS #
 # default security group 
 resource "aws_security_group" "default-sg" {
   name        = "default_sg"
-  vpc_id      = "${aws_vpc.vpc.id}"
+  vpc_id      = aws_vpc.vpc.id
 
   # SSH access from anywhere
   ingress {
